ff05c678474b74e80d2dd4b66d4e2d147c59a5d9
Kaung Satt Hein <kaungsatthein2742@gmail.com>
2025-07-25 21:31:32 +0630

    fix(deployment2): deployment error 2

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,8 @@ import Google from "next-auth/providers/google";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
       authorization: {
         params: {
           prompt: "select_account",
@@ -13,4 +15,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
     }),
   ],
+  secret: process.env.AUTH_SECRET,
+  debug: process.env.NODE_ENV === "development",
 });
