4e9013b12dd074eb9050832b1da6ba433bab326d
Kaung Satt Hein <kaungsatthein2742@gmail.com>
2025-08-20 16:16:29 +0630

    feat (oauth): oauth

--- src/auth.ts
+++ src/auth.ts
@@ -6,14 +6,57 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-      authorization: {
-        params: {
-          prompt: "select_account",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account }) {
+      console.log("user, account", user, account);
+      try {
+        if (user.email && account?.provider === "google") {
+          const response = await fetch(
+            `${process.env.BACKEND_URL}/api/v1/users/oauth`,
+            {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                profile: {
+                  name: user.name,
+                  email: user.email,
+                  image: user.image,
+                  providerId: account.providerAccountId,
+                },
+              }),
+            }
+          );
+
+          if (!response.ok) {
+            console.error(
+              "Failed to sync user with Express API:",
+              response.statusText
+            );
+          } else {
+            const data = await response.json();
+            console.log("User synced successfully:", data.user);
+          }
+        }
+        return true; //
+      } catch (error) {
+        console.error("Failed to sync user with Express API:", error);
+        return true;
+      }
+    },
+    async session({ session, token }) {
+      if (session.user && token.email) {
+        session.user.email = token.email as string;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user && user.email) {
+        token.email = user.email;
+      }
+      return token;
+    },
+  },
   trustHost: true,
 });
